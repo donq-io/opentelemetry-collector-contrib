23fa96254daeac736509a592a13df3564d9a8af6
Federico Serafini <federico@donq.io>
2022-08-19 15:18:49 +0000

    add datadog receiver

--- testbed/stabilitytests/trace_test.go
+++ testbed/stabilitytests/trace_test.go
@@ -124,4 +124,20 @@ func TestStabilityTracesZipkin(t *testing.T) {
 		processorsConfig,
 		nil,
 	)
+
+func TestStabilityTracesDatadog(t *testing.T) {
+	scenarios.Scenario10kItemsPerSecond(
+		t,
+		datasenders.NewDatadogDataSender(),
+		datareceivers.NewDataDogDataReceiver(),
+		testbed.ResourceSpec{
+			ExpectedMaxCPU:      80,
+			ExpectedMaxRAM:      110,
+			ResourceCheckPeriod: resourceCheckPeriod,
+		},
+		contribPerfResultsSummary,
+		processorsConfig,
+		nil,
+	)
+}
 }
